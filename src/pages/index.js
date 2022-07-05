217aa2f204246f7b67e2650c51eb4ae79743733d
Gerardo <gerardo@latteandcode.com>
2022-07-05 22:52:13 +0200

    fix: broken links

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,7 @@ function HomepageHeader() {
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link className="button button--secondary" to="/docs/intro">
+          <Link className="button button--secondary" to="/viajes/intro">
             ¡Nuestros pichuviajes! 🗺
           </Link>
         </div>
